4163f0809b1c4b8187aca43251c61ae0e75be63f
Dr. Drang <drdrang@gmail.com>
2010-12-09 21:10:37 -0600

    Slightly wider content area. New location for Markdown and SmartyPants processors.

--- mail-preview.php
+++ mail-preview.php
@@ -1,7 +1,7 @@
 <?php
 $home = getenv("HOME");
-require_once("$home/php-markdown-extra-math/markdown.php");
-require_once("$home/php-smartypants/smartypants.php");
+require_once("$home/git/php-markdown-extra-math/markdown.php");
+require_once("$home/git/php-smartypants/smartypants.php");
 $full = stream_get_contents(STDIN);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
