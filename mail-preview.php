403909e47003936ca05e9779ea9f08d88d6d8a34
Dr. Drang <drdrang@gmail.com>
2012-10-11 17:18:21 -0500

    Changed input from stdin to message.md file.

--- mail-preview.php
+++ mail-preview.php
@@ -2,7 +2,7 @@
 $home = getenv("HOME");
 require_once("$home/git/php-markdown-extra-math/markdown.php");
 require_once("$home/git/php-smartypants/smartypants.php");
-$full = stream_get_contents(STDIN);
+$full = file_get_contents("$home/git/mail-preview/message.md");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
